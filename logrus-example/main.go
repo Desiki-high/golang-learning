fbb4a9bd8c93690835f1ce60ba2f851d43642dbe
Desiki-high <2448906309@qq.com>
2023-02-28 10:13:59 +0800

    update the logrus example
    Signed-off-by: Desiki-high <2448906309@qq.com>

--- logrus-example/main.go
+++ logrus-example/main.go
@@ -8,7 +8,7 @@ import (
 func init() {
 	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
-
+	log.SetReportCaller(true)
 	// Output to stdout instead of the default stderr
 	// Can be any io.Writer, see below for File example
 	log.SetOutput(os.Stdout)
@@ -19,6 +19,7 @@ func init() {
 }
 
 func main() {
+
 	//this will be ignorance
 	log.WithFields(log.Fields{
 		"animal": "walrus",
@@ -30,8 +31,19 @@ func main() {
 		"number": 122,
 	}).Warn("The group's number increased tremendously!")
 
+	// the logrus.Entry returned from WithFields()
+	contextLogger := log.WithFields(log.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+	contextLogger.Warn("I'll be logged with common and other field")
+
+	// After fatal system will os.exit
 	log.WithFields(log.Fields{
 		"omg":    true,
 		"number": 100,
 	}).Fatal("The ice breaks!")
+
+	// os had exit, so this will not execute
+	contextLogger.Error("Me too")
 }
