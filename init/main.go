82fb10cb0e07d92fbc0705d5c7ad5dc24d08ffff
Desiki-high <2448906309@qq.com>
2023-03-05 18:17:10 +0800

    create init
    Signed-off-by: Desiki-high <2448906309@qq.com>

---
+++ init/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("First")
+}
+
+func main() {
+	fmt.Println("Forth")
+}
+
+func init() {
+	fmt.Println("Second")
+}
+
+func init() {
+	fmt.Println("Third")
+}
